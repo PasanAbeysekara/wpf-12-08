c085db84f290a4b3949ef8ab68fcab6441b94de6
PasanAbeysekara <69195287+PasanAbeysekara@users.noreply.github.com>
2022-12-08 08:10:25 +0530

    initial commit

---
+++ dec-eight/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace dec_eight
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
