b18f8dcbc45aafbf7f0bbd52f6a9d53d314708f1
PasanAbeysekara <69195287+PasanAbeysekara@users.noreply.github.com>
2022-12-08 08:51:13 +0530

    Create "ModelBindingVM" view model

---
+++ dec-eight/ViewModels/ModelBindingVM.cs
@@ -0,0 +1,16 @@
+﻿namespace dec_eight.ViewModels
+{
+    public class ModelBindingVm
+    {
+        public ModelBindingVm()
+        {
+            FirstName = "Tom";
+            LastName = "Brown";
+            Age = 35;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
