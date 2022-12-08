fdd047474dcb972edf90d9ec96f5b74d2bbb5a8a
PasanAbeysekara <69195287+PasanAbeysekara@users.noreply.github.com>
2022-12-08 08:54:01 +0530

    Made data connection from , modelBinding view model -> modelBinding view

---
+++ dec-eight/Views/ModelBindingWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using dec_eight.ViewModels;
+
+namespace dec_eight.Views
+{
+    public partial class ModelBindingWindow : Window
+    {
+        public ModelBindingWindow()
+        {
+            DataContext = new ModelBindingVm();
+            InitializeComponent();
+        }
+    }
+}
